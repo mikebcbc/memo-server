eb2af247701b3318b5afdd1bcae4fbb347b14004
mikebcbc <mikebconstantino@gmail.com>
2017-11-20 18:22:49 -0500

    Removed node_modules, oops.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(cors({origin: CLIENT_ORIGIN}));
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +11,6 @@ app.get('/api/*', (req, res) => {
 	res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log('Listening on port ${PORT}'));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = {app};
