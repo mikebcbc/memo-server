89318228492dff7021923bb2b057b4756fec3a65
mikebcbc <mikebconstantino@gmail.com>
2017-12-04 21:11:38 -0500

    Add 'site' value to data structure

--- models/contents.js
+++ models/contents.js
@@ -5,6 +5,7 @@ const {Topic} = require('./topics');
 const contentSchema = mongoose.Schema({
 	type: {type: String, required: true},
 	title: {type: String, required: true},
+	site: {type: String, required: true},
 	link: {type: String, required: true},
 	related_topic: {type: mongoose.Schema.Types.ObjectId, ref: 'Topic'}
 });
